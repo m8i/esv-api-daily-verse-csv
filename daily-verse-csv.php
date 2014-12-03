a7a978a161776990043d272cf5180e45cd59764e
UMHB Web Services <webservices@umhb.edu>
2014-12-03 09:32:39 -0600

    Fixed some display issues and errors

--- daily-verse-csv.php
+++ daily-verse-csv.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(0);
+
 header("Content-type: text/csv");
 header("Content-Disposition: attachment; filename=verse.csv");
 header("Pragma: no-cache");
@@ -8,12 +10,16 @@ header("Expires: 0");
 echo '"verse","reference"'."\n";
 
 $file=do_post_request('http://www.esvapi.org/v2/rest/dailyVerse?key=IP&output-format=crossway-xml-1.0');
+$file=preg_replace("/\&emdash\;/i", ' - ', $file);
+$file=preg_replace("/\&ldblquot\;/i", '', $file);
 $xml=simplexml_load_string($file);
 
 $reference=$xml->passage->reference;
 $verse="";
 foreach ($xml->passage->content->{'verse-unit'} as $verse_part) {
-		$verse .= (string)$verse_part . " ";
+	$the_verse_part=(string)$verse_part;
+	if (strlen($the_verse_part)<3) { $the_verse_part = (string)$verse_part->woc; }
+		$verse .= $the_verse_part . " ";
 }
 
 // Sometimes HTML tags slip on in there... let's get rid of them
